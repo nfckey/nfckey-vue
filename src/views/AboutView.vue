a25c57873703da4926bff2267f91dacbd2355d8e
Mercuuury <kovshiksa@gmail.com>
2024-03-21 20:14:44 +0300

    feat(views): :sparkles: make storyline responsive on all devices

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -23,11 +23,15 @@ const initializeStoryline = () => {
 
   gsap.set(content, { yPercent: 101 })
 
-  ScrollTrigger.create({
-    trigger: '.story-right',
-    start: 'top top',
-    end: 'bottom bottom',
-    pin: '.story-left'
+  let mm = gsap.matchMedia()
+
+  mm.add('(min-width: 767.98px)', () => {
+    ScrollTrigger.create({
+      trigger: '.story-right',
+      start: 'top top',
+      end: 'bottom bottom',
+      pin: '.story-left'
+    })
   })
 
   imageGroups.forEach((group, index) => {
@@ -164,6 +168,18 @@ onMounted(() => {
     <div class="story-right">
       <div class="story-images">
         <div class="story-images__group">
+          <div class="story-content__section story-content__section_mobile">
+            <p class="story-content__date">Апрель – Май 2023</p>
+            <p class="story-content__title">«Я в деле»</p>
+            <p class="story-content__text">
+              Отправной точкой проекта можно считать участие в конкурсе
+              <a href="https://xn--b1aeda3a0j.xn--p1ai/">«Я в деле»</a> — масштабной программе по
+              развитию предпринимательских навыков среди молодежи. Здесь развилась идея и
+              сформировались первые технические решения. <br /><br />
+              Мы заняли <span>третье место</span> среди всех проектов Москвы в треке «инновации» и
+              впервые познакомились с нашим наставником.
+            </p>
+          </div>
           <img
             src="../assets/images/ya-v-dele-team-1.png"
             alt="Команда NFCKEY на конкурсе «Я в деле»"
@@ -178,6 +194,17 @@ onMounted(() => {
           />
         </div>
         <div class="story-images__group">
+          <div class="story-content__section story-content__section_mobile">
+            <p class="story-content__date">Ноябрь – Декабрь 2023</p>
+            <p class="story-content__title">«Технолидеры Москвы»</p>
+            <p class="story-content__text">
+              В рамках молодежного технологического акселератора
+              <a href="https://rcmos.moscow/technoentr">«Технолидеры Москвы»</a> нас ожидал
+              двухнедельный период продуктивной работы с экспертами. <br /><br />
+              Нам удалось не только получить ценную обратную связь и заметно продвинуться в развитии
+              проекта, но и наладить деловые связи, обратив на себя внимание экспертов и партнеров.
+            </p>
+          </div>
           <img
             src="../assets/images/technolider-1.png"
             alt="Команда NFCKEY на фоне выставочного стенда для конкурса «Технолидеры Москвы»"
@@ -192,8 +219,20 @@ onMounted(() => {
           />
         </div>
         <div class="story-images__group">
+          <div class="story-content__section story-content__section_mobile">
+            <p class="story-content__date">Декабрь 2023 – н. в.</p>
+            <p class="story-content__title">«Долина стартапов»</p>
+            <p class="story-content__text">
+              В декабре мы прошли отбор на технологический акселератор
+              <a href="https://xn--80aaaigyurggkkloc.xn--p1ai/">«Долина стартапов»</a>, созданный
+              при сотрудничестве с двумя лидерами в сфере инноваций: «Акселератором Возможностей» и
+              научно-технологической долиной МГУ «Воробьевы горы». <br /><br />
+              Сейчас происходит определение партнеров на конкурсной основе, а в марте 2024 года
+              ожидается оглашение результатов.
+            </p>
+          </div>
           <img src="../assets/images/ds-logo.svg" alt="Логотип акселератора «Долина стартапов»" />
-          <div class="flex w-full justify-around px-4">
+          <div class="flex w-full justify-around py-8 md:px-4 md:py-0">
             <img
               class="w-[40%]"
               src="../assets/images/ds-mgu.png"
@@ -207,12 +246,54 @@ onMounted(() => {
           </div>
         </div>
         <div class="story-images__group">
+          <div class="story-content__section story-content__section_mobile">
+            <p class="story-content__date">Январь 2024 – н. в.</p>
+            <p class="story-content__title story-content__title_large">
+              «Россия – страна возможностей»
+            </p>
+            <p class="story-content__text">
+              В январе мы приняли участие в национальной премии
+              <a href="https://premia.rsv.ru/">«Россия – страна возможностей»</a>. Организатор
+              мероприятия предоставляет открытую площадку для обмена опытом между талантливыми и
+              неравнодушными людьми всех возрастов. <br /><br />
+              Недавно мы прошли первый отборочный этап и получили сертификат, чему крайне рады ввиду
+              большого числа желающих принять участие.
+            </p>
+          </div>
           <img src="../assets/images/rsv-logo.webp" alt="Логотип «Россия – страна возможностей»" />
         </div>
         <div class="story-images__group">
+          <div class="story-content__section story-content__section_mobile">
+            <p class="story-content__date">Январь 2024 – н. в.</p>
+            <p class="story-content__title">Проект «Твой ход»</p>
+            <p class="story-content__text">
+              В конце января мы подали заявку на участие во всероссийском студенческом проекте
+              <a href="https://tvoyhod.online/">«Твой ход»</a>. Он помогает участникам открывать
+              себя по-новому, получать практические навыки, находить единомышленников, создавать и
+              развивать собственные проекты. <br /><br />
+              В данный момент мы шаг за шагом проходим этапы конкурса и в скором времени сможем
+              поделиться результатами.
+            </p>
+          </div>
           <img src="../assets/images/tvoi-hod-logo.webp" alt="Логотип «Твой ход»" />
         </div>
         <div class="story-images__group">
+          <div class="story-content__section story-content__section_mobile">
+            <p class="story-content__date">Март 2024</p>
+            <p class="story-content__title story-content__title_large">
+              Открытие IV сезона «Я в деле»
+            </p>
+            <p class="story-content__text">
+              В марте мы вернулись на проект
+              <a href="https://xn--b1aeda3a0j.xn--p1ai/">«Я в деле»</a>, но уже в роли экспертов. На
+              мероприятии мы смогли познакомиться с новым поколением предпринимателей, а самое
+              главное - оценить их с экспертной точки зрения и поделиться нашей историей и видением.
+              <br /><br />
+              Несколько дней спустя наши лица появились на экранах
+              <a href="https://dom.fadm.gov.ru/">Дома молодежи ВДНХ</a> во время открытия IV сезона
+              федерального предпринимательского конкурса «Я в деле».
+            </p>
+          </div>
           <img src="../assets/images/nfckey-doorlock-ltr.png" alt="" />
         </div>
       </div>
